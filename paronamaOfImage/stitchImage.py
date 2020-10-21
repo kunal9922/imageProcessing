1b83266e2fbb0cd44f102a926bc684fe3f34a950
kunal <53283003+kunal9922@users.noreply.github.com>
2020-10-21 23:07:38 +0530

    Fetch images From folders

--- paronamaOfImage/stitchImage.py
+++ paronamaOfImage/stitchImage.py
@@ -3,7 +3,15 @@ import os
 
 class stichImage:
 	def __init__(self, folderPath: str):
-		mainFolder = "imagesDataSet"
+		self.mainFolder = folderPath
 		#   fetching all the folder names
-		myFolders = os.listdir(mainFolder)  # return total no. of folders are  in main folder
+		myFolders = os.listdir(self.mainFolder)  # return total no. of folders are  in main folder
 		print(myFolders)
+
+		# if other more folder are in main directory so fetch that
+		for folder in myFolders:
+			path = self.mainFolder + '//'+folder
+			self.images = []  # empty list for storing images
+			myList = os.listdir(path) # return all images for specific path
+			print(myList)
+
