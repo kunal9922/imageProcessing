c5c662c8831df8a92d7c6ed04412bcd7eec7cac4
kunal <53283003+kunal9922@users.noreply.github.com>
2020-10-21 23:27:41 +0530

    proper getting images for paronama img from folders

--- paronamaOfImage/stitchImage.py
+++ paronamaOfImage/stitchImage.py
@@ -3,7 +3,8 @@ import os
 
 class stichImage:
 	def __init__(self, folderPath: str):
-		self.mainFolder = folderPath
+		self.mainFolder = r"E:\imageProcessing\paronamaOfImage\ImagesDataSet"
+
 		#   fetching all the folder names
 		myFolders = os.listdir(self.mainFolder)  # return total no. of folders are  in main folder
 		print(myFolders)
@@ -13,5 +14,13 @@ class stichImage:
 			path = self.mainFolder + '//'+folder
 			self.images = []  # empty list for storing images
 			myList = os.listdir(path) # return all images for specific path
-			print(myList)
+			print(f"Total images are detected {len(myList)}")
+			for imgN in myList:
+					currImg = cv.imread(f'{path}//{imgN}')
+					#currImg = cv.resize() # resizing for show in window screen properly.
+					self.images.append(currImg)
+					print(self.images)
 
+
+if __name__ == '__main__':
+	paronama = stichImage("")
